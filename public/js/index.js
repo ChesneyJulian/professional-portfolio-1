57eed196825673c0d3268bf8bb2217cc2672182b
Chesney Julian <chesney.julian724@gmail.com>
2023-08-11 13:59:47 -0400

    begin creating cards for recent projects and add images for projects to public directory images folder

--- public/js/index.js
+++ public/js/index.js
@@ -1,15 +1,14 @@
 const sidebar = document.getElementById("sidebar");
 const sidebarDisplay = sidebar.style.display;
 
-var myScrollFunc = function() {
-  var y = window.scrollY;
-  if (y >= 300) {
+var myScrollFunc = async function () {
+  if (window.scrollY >= 600) {
     if (sidebar.style.display === 'none') {
-      sidebar.setAttribute('class', 'delay-[300ms] duration-[400ms] taos:translate-x-[200px] taos:opacity-0 aos item fixed top-0 right-0 z-40 w-64 h-screen')
+      sidebar.setAttribute('class', 'delay-[300ms] duration-[400ms] ease-[cubic-bezier(.47,0,.745,.715)] taos:translate-x-[200px] taos:opacity-0 aos item fixed top-0 right-0 z-40 w-24 lg:w-64 h-screen')
       sidebar.setAttribute('style', 'display:flex;')
     };
   } else {
-     sidebar.setAttribute('style', 'display:none;')
+     sidebar.setAttribute('class', 'fade-out aos item fixed top-0 right-0 z-40 w-64 h-screen')
   }
 };
 
