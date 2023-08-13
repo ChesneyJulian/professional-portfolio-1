e1678bbc8a5744c1ba8c1af946f53e8224f1173f
Chesney Julian <chesney.julian724@gmail.com>
2023-08-13 17:05:27 -0400

    add more content to cards and start playing around with card format. Planning to separate images from cards for more continuous feel of ui

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,7 @@
 const sidebar = document.getElementById("sidebar");
 const sidebarDisplay = sidebar.style.display;
 
-var myScrollFunc = async function () {
+const myScrollFunc = () => {
   if (window.scrollY >= 600) {
     if (sidebar.style.display === 'none') {
       sidebar.setAttribute('class', 'delay-[300ms] duration-[400ms] ease-[cubic-bezier(.47,0,.745,.715)] taos:translate-x-[200px] taos:opacity-0 aos item fixed top-0 right-0 z-40 w-24 lg:w-64 h-screen')
@@ -12,6 +12,9 @@ var myScrollFunc = async function () {
   }
 };
 
+const createCardInfo = () => {
+
+}
 
 window.addEventListener("scroll", myScrollFunc);
 
