2580d20f2c538ebd631c26e6269e0ebf4324a1c1
Chesney Julian <chesney.julian724@gmail.com>
2023-08-14 18:59:51 -0400

    begin adjusting layout and styling to align with half and half functionality in large screen

--- public/js/index.js
+++ public/js/index.js
@@ -1,18 +1,4 @@
-const sidebar = document.getElementById("sidebar");
-const sidebarDisplay = sidebar.style.display;
 
-const myScrollFunc = () => {
-  if (window.scrollY >= 600) {
-    if (sidebar.style.display === 'none') {
-      sidebar.setAttribute('class', 'delay-[300ms] duration-[400ms] ease-[cubic-bezier(.47,0,.745,.715)] taos:translate-x-[200px] taos:opacity-0 aos item fixed top-0 right-0 z-40 w-24 lg:w-64 h-screen')
-      sidebar.setAttribute('style', 'display:flex;')
-    };
-  } else {
-     sidebar.setAttribute('class', 'fade-out aos item fixed top-0 right-0 z-40 w-64 h-screen')
-  }
-};
-
-window.addEventListener("scroll", myScrollFunc);
 
 
 
