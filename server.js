35b696281a3b90757dc34eac269538b0abb2afd0
Chesney Julian <chesney.julian724@gmail.com>
2023-08-21 15:52:04 -0400

    correct port typo in server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const exphbs = require('express-handlebars');
 
 const app = express();
-const PORT = process.env.port ?? 3001;
+const PORT = process.env.PORT ?? 3001;
 const hbs = exphbs.create({ });
 const routes = require('./controllers');
 
