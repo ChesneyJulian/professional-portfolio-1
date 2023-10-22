807bea3f4049bd46d7d3fcac2f30f7bf931d65fc
Chesney Julian <chesney.julian724@gmail.com>
2023-10-22 13:15:11 -0400

    add icons to skills and add horizontal rule using flowbite; continue to debug console error for flowbite in future

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,8 @@
 module.exports = {
   content: {
     relative: true,
-    transform: (content) => content.replace(/taos:/g, ''),
-    files: ["./{views,public}/**/*.{handlebars,js}"],
+    // transform: (content) => content.replace(/taos:/g, ''),
+    files: ["./{views,public}/**/*.{handlebars,js}", "./node_modules/flowbite/**/*.js"],
   },
   theme: {
     extend: {
@@ -18,12 +18,14 @@ module.exports = {
     },
   },
   plugins: [
-    require('taos/plugin')
+    // require('taos/plugin')
+    require('flowbite/plugin')
+
   ],
-  safelist: [
-    '!duration-[0ms]',
-    '!delay-[0ms]',
-    'html.js :where([class*="taos:"]:not(.taos-init))'
-  ],
+  // safelist: [
+  //   '!duration-[0ms]',
+  //   '!delay-[0ms]',
+  //   'html.js :where([class*="taos:"]:not(.taos-init))'
+  // ],
 }
 
